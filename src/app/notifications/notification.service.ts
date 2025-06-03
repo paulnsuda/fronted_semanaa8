2f2d589cd8e0e43c9921f898061747a4c3017920
paul_narvaez <jpnarvaez@sudamericano.edu.ec>
2025-06-03 18:40:41 -0500

    fronted f1

--- src/app/notifications/notification.service.ts
+++ src/app/notifications/notification.service.ts
@@ -3,12 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
-  private apiUrl = '${environment.apiUrl}/notifications';
+  private apiUrl = `${environment.apiUrl}/notifications`;
 
   constructor(private http: HttpClient) {}
 
